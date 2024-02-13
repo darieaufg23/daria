12be83d97114e6d9c9768ea35ca3207dd72c4bbb
Дарина <darina@MacBook-Air-Darina.local>
2024-02-13 09:50:17 +0100

    Your commit message here

---
+++ HOmeworks_go_it/Pizza_site/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+if __name__ == '__main__':
+    app.run()
+    
